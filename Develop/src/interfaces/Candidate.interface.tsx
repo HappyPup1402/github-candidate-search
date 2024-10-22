48ccc495eeab09e8ba2459b53341213686a7d474
Izaac Ramirez <izaacramirez@Izaacs-Laptop.local>
2024-10-21 22:06:03 -0700

    created the interface for a candidate

--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,10 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export interface Candidate {
+    name: string;
+    avatar_url: string;
+    location: string | null;
+    email: string | null;
+    company: string | null;
+    bio: string | null;
+  }
+  
